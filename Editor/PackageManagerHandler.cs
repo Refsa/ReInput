8f07a3c6d9fe0c722f3ea9f189384e9a9c49b459
Ole <refsa22@gmail.com>
2020-12-19 11:56:55 +0100

    feat: better bootstrap for editor setup

--- Editor/PackageManagerHandler.cs
+++ Editor/PackageManagerHandler.cs
@@ -2,6 +2,8 @@
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+using System.Linq;
+using System.IO;
 
 namespace Refsa.ReInput.Editor
 {
@@ -13,64 +15,124 @@ namespace Refsa.ReInput.Editor
         static ListRequest listRequest;
 
         static PackRequest packRequest;
+        static AddRequest addRequest;
 
-        static string[ ] wantedPackages = new string[ ]
+        static string[] wantedPackages = new string[]
         {
             "com.unity.inputsystem"
         };
 
         static int currentPackage = 0;
 
-        static ReInputPackageHandler ( )
+        static ReInputPackageHandler()
         {
             currentPackage = 0;
 
-            //listRequest = Client.List ( );
-            //EditorApplication.update += FindPackages;
+            listRequest = Client.List();
+            EditorApplication.update += FindPackages;
         }
 
-        static void FindPackages ( )
+        static void FindPackages()
         {
             if (listRequest.IsCompleted)
             {
                 if (listRequest.Status == StatusCode.Success)
                 {
-                    foreach (var package in listRequest.Result)
+                    foreach (var wantedPackage in wantedPackages)
                     {
-                        Debug.Log (package.name);
+                        if (!listRequest.Result.Any(e => e.name == wantedPackage))
+                        {
+                            addRequest = Client.Add(wantedPackage);
+                            EditorApplication.update += WaitForPackageInstall;
+                        }
                     }
                 }
                 else if (listRequest.Status == StatusCode.Failure)
                 {
-                    Debug.Log (listRequest.Error.message);
+                    Debug.Log("ReInput: " + listRequest.Error.message);
                 }
 
                 EditorApplication.update -= FindPackages;
             }
         }
 
-#if ReInputDev
-        [MenuItem ("Tools/ReInput/Create ReInput Package")]
-#endif
-        static void CreatePackage ( )
-        {
-            packRequest = Client.Pack ("Assets/ReInput/", "Assets/Release/");
-
-            EditorApplication.update += WaitForPacking;
-        }
-
-        static void WaitForPacking ( )
+        static void WaitForPackageInstall()
         {
             if (packRequest.IsCompleted)
             {
                 if (packRequest.Status == StatusCode.Failure)
                 {
-                    Debug.Log (packRequest.Error.message);
+                    Debug.Log(packRequest.Error.message);
                 }
                 else if (packRequest.Status == StatusCode.Success)
                 {
-                    Debug.Log (packRequest.Result.tarballPath);
-                    AssetDatabase.Refresh ( );
+                    ReplaceInputManagerSettings();
+                }
+            }
+
+            EditorApplication.update -= WaitForPackageInstall;
+        }
+
+        [MenuItem("Tools/ReInput/Setup")]
+        static void Setup()
+        {
+            FindPackages();
+            ReplaceInputManagerSettings();
+        }
+
+        static void ReplaceInputManagerSettings()
+        {
+            var settingsPath = LocateFile(Application.dataPath + "/", "InputManager.asset");
+            var isPath = Application.dataPath.Replace("/Assets", "/ProjectSettings/InputManager.asset");
+
+            File.Replace(settingsPath, isPath, "InputManager.asset.old");
+            File.Copy(isPath, settingsPath);
+        }
+        
+        static string LocateFile(string path, string fileName)
+        {
+            foreach (var file in Directory.GetFiles(path))
+            {
+                if (Path.GetFileName(file) == fileName)
+                {
+                    return file;
+                }
+            }
+
+            foreach (var folder in Directory.GetDirectories(path))
+            {
+                var file = LocateFile(folder, fileName);
+                if (!string.IsNullOrEmpty(file))
+                {
+                    return file;
+                }
+            }
+
+            return "";
+        }
+
+#if ReInputDev
+        [MenuItem ("Tools/ReInput/Create ReInput Package")]
+#endif
+        static void CreatePackage()
+        {
+            packRequest = Client.Pack("Assets/ReInput/", "Assets/Release/");
+
+            EditorApplication.update += WaitForPacking;
+        }
+
+        static void WaitForPacking()
+        {
+            if (packRequest.IsCompleted)
+            {
+                if (packRequest.Status == StatusCode.Failure)
+                {
+                    Debug.Log(packRequest.Error.message);
+                }
+                else if (packRequest.Status == StatusCode.Success)
+                {
+                    Debug.Log(packRequest.Result.tarballPath);
+                    AssetDatabase.Refresh();
                 }
 
                 EditorApplication.update -= WaitForPacking;
